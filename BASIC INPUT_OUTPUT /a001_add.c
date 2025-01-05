0177a80165fa21a6f9fb111ab808c71cae40e8fc
Ajay Singh <ajaysingh75121@gmail.com>
2025-01-05 12:01:17 +0530

    Create a001_add.c

---
+++ BASIC INPUT_OUTPUT /a001_add.c
@@ -0,0 +1,19 @@
+// INPUT TWO NUMBER AND ADD
+#include<stdio.h>
+int main()
+{
+    int a,b,s;
+    printf(" enter any number a:");
+    scanf("%d",&a);
+    printf(" enter any number b:");
+    scanf("%d",&b);
+    s= a+b;
+    printf("add two number: %d",s);
+    return 0;
+}
+
+
+//*** OUTPUT--
+//enter any number a:8
+// enter any number b:9
+//add two number: 17*
